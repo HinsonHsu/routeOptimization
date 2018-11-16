a91d562c4b602a62174f8704ce65f6645e854561
hinson <hinson@hinsondeMacBook-Pro.local>
2018-11-16 09:00:22 +0800

    20181116

--- main.py
+++ main.py
@@ -1,48 +1,39 @@
 import numpy as np
 import random
 
-
-
 homeNum = 1 # 车场个数
-vertexHome = np.zeros(shape=(homeNum, 2)); # 默认车场位置为0,0
-windowsHome = np.zeros(shape=(homeNum, 2)); # 车场工作时间
-
+carNum = 3 # 车型个数
 clientNum = 5 # 客户个数
-n = 4 # 垃圾种类数
-garbageNum = 4
+garbageNum = 4 #垃圾种类数
 chargerNum = 3 #充电桩个数
 #每个客户的垃圾重量规格矩阵
-clientWeightMatrix = np.zeros(shape=(clientNum,n))
-#每个客户所在位置
-vertexClient = np.zeros(shape=(clientNum,2))
+clientWeightMatrix = np.zeros(shape=(clientNum, garbageNum))
 #每个客户的时间窗
 clientWindow = np.zeros(shape=(clientNum, 2))
 #每个客户需要的服务时间
 clientServeTime = np.zeros(shape=(clientNum, 1))
-#每个充电桩的位置
-vertextCharger = np.zeros(shape=(chargerNum, n))
-carNum = 2 # 车型个数
 #每种车型的垃圾容量矩阵
-collectorMatrix = np.zeros(shape=(carNum, n))
-#工作时长
+collectorMatrix = np.zeros(shape=(carNum, garbageNum))
+#每种车型工作时长
 carServeTime = np.zeros(shape=(carNum, 1))
-#电池规格
+#每种车型电池规格
 carPowerCapacity = np.zeros(shape=(carNum, 1))
-#车子速度
-v = 1
+#车子运行速度
+moveSpeed = 1
+#充电速度
+chargeSpeed = 1
 
 antNum = 10 # 蚂蚁个数
 times = 100 # 迭代次数
 
 #节点个数，车场+客户+充电桩
 nodeNum = homeNum + clientNum + chargerNum 
-#信息素浓度矩阵（边矩阵 nodeNum * nodeNum)
-edgeMatrix = np.zeros(shape=(nodeNum, 2))
+#距离矩阵（边矩阵 nodeNum * nodeNum)，第0个代表车场，第1到homeNum个为客户，第homeNum+1到nodeNum-1为充电桩
+distanceMatrix = np.zeros(shape=(nodeNum, nodeNum))
+#信息素浓度矩阵，同上
+distanceMatrix = np.zeros(shape=(nodeNum, nodeNum))
 
-#距离矩阵
-distanceMatrix = np.zeros(shape=(nodeNum, 2))
 
-currentMatrix = np.zeros(shape=(1, n))
 
 def init():# 变量初始化
     
